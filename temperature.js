adff161255d6ca82c9c1afd6290a5da27ed3e00e
Sebastian Czech <sebaczech@gmail.com>
2019-08-18 04:32:23 +0200

    Display temperature of CPU

---
+++ temperature.js
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Duck)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Square)
+})
+basic.showIcon(IconNames.Scissors)
+basic.forever(function () {
+	
+})
+
